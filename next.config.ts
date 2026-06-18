dd147cc1d1ab05575b8a298c1714e87635ee6852
cc <331201489@qq.com>
2026-06-18 10:58:31 +0800

    chore

--- next.config.ts
+++ next.config.ts
@@ -11,22 +11,14 @@
 // export default nextConfig;
 
 /** @type {import('next').NextConfig} */
-
-// 如果是“项目主页”，需要配置 basePath；如果是“用户主页”，留空字符串即可
-const repo = 'shihuiming-website' // 例如 'my-project'
-
 const nextConfig = {
-  // 1. 启用静态导出
   output: 'export',
-
-  // 2. 禁用默认图片优化（GitHub Pages 不支持 Node.js 服务器优化图片）
   images: {
-    unoptimized: true,
+    unoptimized: true, // 必须加，禁用默认图片优化
   },
-
-  // 3. 配置基础路径（⚠️ 如果是“用户主页”仓库，请删除下面这两行！）
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}`,
+  // 下面这两行非常重要，对应你的仓库名！
+  basePath: '/shihuiming-website',
+  assetPrefix: '/shihuiming-website',
 }
 
 module.exports = nextConfig
